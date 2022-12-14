7cee56a8ecbba628295d6d746b42d86e8e861cd3
Sahadat Hossen <sahadatjh@gmail.com>
2022-12-14 23:39:06 +0600

    add new STUDENT component

--- component-reuse/src/components/movie.component.js
+++ component-reuse/src/components/movie.component.js
@@ -4,12 +4,12 @@ import Rating from './rating.component';
 class Movies extends Component {
     state = { 
         movies:[
-            {rank:1, title:"The Message", imdb_rating:'9.2', your_rating:true, action:''},
-            {rank:2, title:"Three idiots", imdb_rating:'9.1', your_rating:false, action:''},
-            {rank:3, title:"Iron Man", imdb_rating:'9.0', your_rating:false, action:''},
-            {rank:4, title:"Robot", imdb_rating:'8.0', your_rating:true, action:''},
-            {rank:5, title:"Dui Musafir", imdb_rating:'8.9', your_rating:false, action:''},
-            {rank:6, title:"Hawa", imdb_rating:'8.8', your_rating:false, action:''},
+            {rank:1, title:"The Message", imdb_rating:'9.2', your_rating:true, action:'X'},
+            {rank:2, title:"Three idiots", imdb_rating:'9.1', your_rating:false, action:'X'},
+            {rank:3, title:"Iron Man", imdb_rating:'9.0', your_rating:false, action:'X'},
+            {rank:4, title:"Robot", imdb_rating:'8.0', your_rating:true, action:'X'},
+            {rank:5, title:"Dui Musafir", imdb_rating:'8.9', your_rating:false, action:'X'},
+            {rank:6, title:"Hawa", imdb_rating:'8.8', your_rating:false, action:'X'},
         ]
     } 
 
@@ -20,13 +20,19 @@ class Movies extends Component {
         this.setState({ movies });
     }
 
+    handleRemove = (movieRank) => {
+        const movies = [...this.state.movies];
+        const UpdateMovies = movies.filter( (movie) => movie.rank !== movieRank );
+        this.setState({ movies : UpdateMovies});
+    }
+
     render() {
         const columns=[
-            {label:"Rank", path:'rank', content: (movie, key) => <td>{movie.rank}</td>},
-            {label:"Title", path:'title', content: (movie, key) => <td>{movie.title}</td>},
-            {label:"IMDB Rating", path:'imdb_rating', content: (movie, key) => <td>{movie.imdb_rating}</td>},
-            {label:"Your Rating", path:'your_rating', content: (movie, key) => <td><Rating key={movie.rank} isRated={movie.your_rating} handleToggleRatting={this.handleToggleRatting} rank={movie.rank} /></td>},
-            {label:"Action", path:'action', content: (movie, key) => <td>{movie.action}</td>}
+            {label:"Rank", path:'rank', content: (movie, key) => <td>{movie[key]}</td>},
+            {label:"Title", path:'title', content: (movie, key) => <td>{movie[key]}</td>},
+            {label:"IMDB Rating", path:'imdb_rating', content: (movie, key) => <td>{movie[key]}</td>},
+            {label:"Your Rating", path:'your_rating', content: (movie, key) => <td><Rating key={movie.rank} isRated={movie[key]} handleToggleRatting={this.handleToggleRatting} rank={movie.rank} /></td>},
+            {label:"Action", path:'action', content: (movie, key) => <td><button className='btn btn-danger btn-sm' >{movie[key]}</button></td>}
         ] 
         return (
             <Table columns={columns} info={this.state.movies} />
