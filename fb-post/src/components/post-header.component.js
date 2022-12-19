61ec37d42372a25ed37ba1c3096ea052f487af61
Sahadat Hossen <sahadatjh@gmail.com>
2022-12-20 00:11:39 +0600

    delete btn UI modification

--- fb-post/src/components/post-header.component.js
+++ fb-post/src/components/post-header.component.js
@@ -4,7 +4,7 @@ const PostHeader = ({ info, handleRemove }) => {
     return ( 
         <React.Fragment>
             <span className='h4 text-uppercase'>{info.title}</span>
-            <button className='float-end mt-2 mx-2 h2 btn btn-outline-secondary' title="Delete this post." onClick={ () => handleRemove(info.id)}>X</button>
+            <button className='float-end mt-2 mx-2 h2 btn btn-outline-danger' title="Delete this post." onClick={ () => handleRemove(info.id)}><i className="bi bi-trash"></i></button>
             <p className='text-muted m-0'>{info.date}</p>
         </React.Fragment>
      );
